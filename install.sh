217ff394176a3596120ae51b4d1a9905d1c80e33
Celebrian <per_kb@hotmail.com>
2020-10-08 21:25:35 +0200

    Removed debug text, added -x to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Find all files in this directory starting with a period.
 # If that file is .gitignore, continue without doing anything.
@@ -7,18 +7,13 @@
 # If the file does not exist at all, just create a symlink.
 # If none of these things happen, write error message.
 for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n"); do
-    echo "Filename: $file"
     if [[ "$file" == ".gitignore" ]]; then
-	echo "Skipping .gitignore."
         continue
     elif [[ -L ~/$file ]]; then
-	echo "Skipping $file; already a symlink."
 	continue
     elif [[ -f ~/$file ]]; then
-	echo "Creating backup and symlink of $file."
 	ln -sb $PWD/$file ~/$file
     elif [[ ! -e ~/$file ]]; then
-	echo "Creating symlink for $file, as no backup or file could be found."
 	ln -s $PWD/$file ~/$file
     else
 	echo "$file exists, but is not a regular file or a symlink"
@@ -27,6 +22,5 @@ done
 
 # If vimrc is a symlink, and we do not have .vim folder, we also want the folder with plugins
 if [[ -L ~/.vimrc && ! -L ~/.vim ]]; then
-    echo ".vimrc is a symlink, so add .vim folder with vim plugin manager."
     ln -s $PWD/.vim ~/.vim
 fi
